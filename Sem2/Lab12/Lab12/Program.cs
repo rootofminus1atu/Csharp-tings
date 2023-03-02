d94fa740d44bf2e5f2eb774d41fd5288418f0a3f
s00240122 <s00240122@atu.ie>
2023-03-02 15:20:05 +0000

    new lab

---
+++ Sem2/Lab12/Lab12/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Lab12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // how tf do I add decorators to C#
+            var main = new ConcreteComponent();
+            var decoratorA = new ConcreteDecoratorA(main);
+            var decoratorB = new ConcreteDecoratorB(main);
+
+            decoratorB.Operation();
+        }
+    }
+
+    interface IComponent
+    {
+        void Operation();
+    }
+
+    class ConcreteComponent : IComponent
+    {
+        public void Operation()
+        {
+            Console.WriteLine("ConcreteComponent operation");
+        }
+    }
+
+    abstract class Decorator : IComponent
+    {
+        protected IComponent component;
+
+        public Decorator(IComponent component)
+        {
+            this.component = component;
+        }
+
+        public virtual void Operation()
+        {
+            component.Operation();
+        }
+    }
+
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(IComponent component) : base(component)
+        {
+        }
+
+        public override void Operation()
+        {
+            AddedBehavior();
+            base.Operation();
+        }
+
+        private void AddedBehavior()
+        {
+            Console.WriteLine("ConcreteDecoratorA AddedBehavior");
+        }
+    }
+
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(IComponent component) : base(component)
+        {
+        }
+
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+        }
+
+        private void AddedBehavior()
+        {
+            Console.WriteLine("ConcreteDecoratorB AddedBehavior");
+        }
+    }
+}
---
+++ Sem2/Lab12/Lab12Q1/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace Lab12Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc = new BankAccount(
+                customerName: "john",
+                type: "Current",
+                BIC: 123456789,
+                number: 999888777,
+                balance: 1000,
+                PIN: 1234
+                );
+
+            Console.WriteLine(acc.balance);
+
+            BankAccount.Withdraw(acc, 500);
+
+
+            Console.WriteLine(acc.balance);
+        }
+    }
+
+    public class BankAccount
+    {
+        public string customerName { get; set; }
+        public string type { get; set; }
+        public int BIC { get; set; }
+        public int number { get; set; }
+        public double balance { get; set; }
+        public int PIN { get; set; }
+
+        public BankAccount(string customerName, string type, int BIC, int number, double balance, int PIN)
+        {
+            this.customerName = customerName;
+            // add checks for the correct account type
+            this.type = type;
+            this.BIC = BIC;
+            this.number = number;
+            this.balance = balance;
+            this.PIN = PIN;
+        }
+
+
+        public void Lodge(double amount)
+        {
+            // what does lodge mean?
+        }
+
+        // check return void 
+
+        public static bool CheckPIN(BankAccount owner)
+        {
+            const int TRIES = 3;
+
+            for (int i = 0; i < TRIES; i++)
+            {
+                Console.Write("Input PIN: ");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input == owner.PIN)
+                    return true;
+                else
+                    Console.WriteLine($"{TRIES - i - 1} tries left.");
+            }
+
+            Console.WriteLine("\nno");
+            return false;
+        }
+
+
+        public static void Withdraw(BankAccount owner, double amount)
+        {
+            if (BankAccount.CheckPIN(owner))
+            {
+                if (amount > owner.balance)
+                    Console.WriteLine("Insufficient funds");
+                else
+                    owner.balance -= amount;
+            }
+            else
+                Console.WriteLine("\nno");
+        }
+
+        public void Withdraw(double amount)
+        {
+            // check pin
+            // and then
+
+            // do withdrawal stuff
+        }
+
+        public void CheckBalance()
+        {
+            const int TRIES = 3;
+
+            for (int i = 0; i < TRIES; i++)
+            {
+                Console.Write("Input PIN: ");
+                int input = int.Parse(Console.ReadLine());
+                if (input == this.PIN)
+                {
+                    // do a balance check
+                    // or do a withdrawal
+                    // or do something else
+                    //
+                    // how do I wrap each method with this for-if-else PIN checking
+
+
+
+                    // only this part
+                    Console.WriteLine($"Your current balance is: {this.balance}");
+                    // is the functionality
+
+
+
+                    break;
+                }
+                else
+                    Console.WriteLine($"Invalid PIN | {TRIES - i - 1} tries left");
+            }
+        }
+
+        public void ChangePIN(int newPIN)
+        {
+            this.PIN = newPIN;
+        }
+    }
+}
