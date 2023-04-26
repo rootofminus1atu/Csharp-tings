6fbdfb9a2c77dbe14e4ebf5667bc70a936799e67
s00240122 <s00240122@atu.ie>
2023-04-26 17:45:24 +0100

    more decorators

--- Sem2/Lab14/Lab14Q2/Program.cs
+++ Sem2/Lab14/Lab14Q2/Program.cs
@@ -6,6 +6,12 @@ namespace Lab14Q2
     {
         static void Main(string[] args)
         {
+            // actions, functions and DECORATORS
+            // here they actually work
+            // note: they work best with actions
+            // it's possible to make them work with general functions too
+            // although that's more challenging
+
             var hi = (int x) => x*x ;
             Func<int, int> hi2 = x => x * x;
             Func<int, int, int> hi3 = (x, y) => x * y;
@@ -17,7 +23,28 @@ namespace Lab14Q2
 
             Decorator(() => Say("Hello"))();
 
-            // test
+            // paramerator with 1 return value
+
+            Func<int> test = () => 5;
+            test = Paramerator1(test);
+
+            var x = test();
+            Console.WriteLine(x);
+
+            // paramerator with 1 input and 1 return value
+
+            Func<int, int> invert = x => -x;
+            invert = Paramerator2(invert);
+
+            var y = invert(6);
+
+
+            // WHY ???
+            // Double = Paramerator2(Double);
+            // why not work
+            Func<int, int> doubleDecorated = Paramerator2(Double);
+            Console.WriteLine(doubleDecorated(7));
+
         }
 
         static void SayHi()
@@ -25,9 +52,21 @@ namespace Lab14Q2
             Console.WriteLine("Hi");
         }
 
+
+
         static void Say(string str)
         {
             Console.WriteLine($"I say {str}");
+        }
+
+        static int Double(int num)
+        {
+            return 2 * num;
+        }
+
+        static int DecoratedDouble(int num)
+        {
+            return Paramerator2(Double)(num);
         }
 
         static Action Decorator(Action action)
@@ -42,11 +81,49 @@ namespace Lab14Q2
             return wrapper;
         }
 
-        static Func<T> Paramerator<T>(Func<T> func)
+        static Func<T> Paramerator1<T>(Func<T> func)
         {
-            return func;
+            Func<T> wrapper = () =>
+            {
+                Console.WriteLine("started");
+                var result = func();
+                Console.WriteLine(result);
+                Console.WriteLine("ended");
+                return result;
+            };
+
+            return wrapper;
         }
 
-        
+
+        static Func<T, TResult> Paramerator2<T, TResult>(Func<T, TResult> func)
+        {
+            Func<T, TResult> wrapper = (T arg) =>
+            {
+                Console.WriteLine("started");
+                var result = func(arg);
+                Console.WriteLine(result);
+                Console.WriteLine("ended");
+                return result;
+            };
+
+            return wrapper;
+        }
+
+        static Func<int, int> Paramerator2(Func<int, int> func)
+        {
+            Func<int, int> wrapper = (int arg) =>
+            {
+                Console.WriteLine("started");
+                var result = func(arg);
+                Console.WriteLine(result);
+                Console.WriteLine("ended");
+                return result;
+            };
+
+            return wrapper;
+        }
+
+
     }
 }
