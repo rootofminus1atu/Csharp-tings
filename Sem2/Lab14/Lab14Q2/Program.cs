1c4fcaf156118406d02d7029d02e6c165d804170
s00240122 <s00240122@atu.ie>
2023-04-26 14:38:35 +0100

    Update Program.cs

--- Sem2/Lab14/Lab14Q2/Program.cs
+++ Sem2/Lab14/Lab14Q2/Program.cs
@@ -1,15 +1,52 @@
-﻿namespace Lab14Q2
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lab14Q2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            char copyrightSymbol = '\u00A9';
-            char omegaSymbol = '\u03A9';
-            char newLine = '\u000A';
+            var hi = (int x) => x*x ;
+            Func<int, int> hi2 = x => x * x;
+            Func<int, int, int> hi3 = (x, y) => x * y;
+            Func<int, int, (int, int)> hi4 = (x, y) => (y, x);
+            Func<int, int, int, int> hi5 = (x, y, z) => x + y + z;
 
-            Console.WriteLine($"{copyrightSymbol} and {omegaSymbol} and {newLine} the end");
+            var fancy = Decorator(SayHi);
+            fancy();
 
+            Decorator(() => Say("Hello"))();
+
+            // test
         }
+
+        static void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        static void Say(string str)
+        {
+            Console.WriteLine($"I say {str}");
+        }
+
+        static Action Decorator(Action action)
+        {
+            var wrapper = () =>
+            {
+                Console.WriteLine("started");
+                action();
+                Console.WriteLine("ended");
+            };
+
+            return wrapper;
+        }
+
+        static Func<T> Paramerator<T>(Func<T> func)
+        {
+            return func;
+        }
+
+        
     }
 }
