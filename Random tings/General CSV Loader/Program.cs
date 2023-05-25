e7fc82d32e69da53f0e3ba3495a1f04048dbb625
RootOfMinus1ATU <s00240122@atu.ie>
2023-05-25 20:28:38 +0100

    added csv converter

---
+++ Random tings/General CSV Loader/Program.cs
@@ -0,0 +1,152 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace General_CSV_Loader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            string filePath = @"../../../people.txt";
+
+            List<Person> people2 = ExtractData<Person>(filePath);
+            Console.WriteLine(people2[1].ToString());
+        }
+
+        public static T LineToObj<T>(string line)
+        {
+            string[] fields = line.Split(',');
+            T obj = Activator.CreateInstance<T>();
+
+            var properties = typeof(T).GetProperties();
+
+            for (int i = 0; i < Math.Min(properties.Length, fields.Length); i++)
+            {
+                PropertyInfo property = properties[i];
+                property.SetValue(obj, Convert.ChangeType(fields[i], property.PropertyType));
+            }
+
+            return obj;
+        }
+
+        public static List<T> ConvertToList<T>(List<string> contents)
+        {
+            List<T> objects = new List<T>() { };
+
+            foreach (string line in contents)
+            {
+                T obj = LineToObj<T>(line);
+
+                objects.Add(obj);
+            }
+
+            return objects;
+        }
+
+        public static List<string> LoadCSV(string filePath, bool skip1stRow = false)
+        {
+            List<string> lines = new List<string>() { };
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Loading CSV file contents...");
+            Console.ResetColor();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(filePath))
+                {
+                    // skipping the 1st row
+                    if (skip1stRow)
+                        sr.ReadLine();
+
+                    string? line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+
+                        // debug
+                        Console.WriteLine(line);
+                    }
+
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("File contents loaded successfully!");
+                Console.ResetColor();
+            }
+            catch (Exception ex) when (
+                ex is DirectoryNotFoundException ||
+                ex is FileNotFoundException ||
+                ex is UnauthorizedAccessException ||
+                ex is IOException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"[ERROR] {ex.Message}");
+                Console.ResetColor();
+
+                Environment.Exit(0);
+            }
+
+            return lines;
+        }
+
+        public static List<T> ExtractData<T>(string filePath, bool skip1stRow = false)
+        {
+            List<string> contents = LoadCSV(filePath, skip1stRow);
+
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            int propertiesCount = properties.Length;
+            int fieldsCount = contents[0].Split(',').Length;
+
+            if (fieldsCount < propertiesCount)
+            {
+                PropertyInfo[] unneededProps = properties[fieldsCount..];
+                string[] unneededNames = unneededProps.Select(x => x.Name).ToArray();
+                string unneededNamesStr = string.Join(", ", unneededNames);
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"[Warning] {typeof(T).Name} has {propertiesCount} properties, but there are only {fieldsCount} fields in the CSV file. Consider removing `{unneededNamesStr}` or else they'll be initialized to default values.");
+                Console.ResetColor();
+            }
+            else if (fieldsCount > propertiesCount)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"[Warning] The CSV file has {fieldsCount} fields, but there are only {propertiesCount} properties in the {typeof(T).Name} class. Consider adding more properties to {typeof(T)}");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Converting contents to");
+                Console.ResetColor();
+            }
+
+
+            return ConvertToList<T>(contents);
+        }
+
+
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+
+        public Person() { }
+
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
