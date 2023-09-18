4903c5be76c79663e4201d1fe618f7413da87398
RootOfMinus1ATU <s00240122@atu.ie>
2023-09-18 20:46:37 +0100

    sem3

---
+++ Sem3/Lab1/Q1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using System.Runtime;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("Input percentage: ");
+                int percentage = int.Parse(Console.ReadLine());
+
+                string grade = GetGrade(percentage);
+                int points = GetPoints(grade);
+
+                Console.WriteLine($"Percentage: {percentage}\nGrade: {grade}\nPoints: {points}");
+
+                total += points;
+            }
+
+            Console.WriteLine($"Total: {total}");
+
+
+        }
+
+        static string GetGrade(int percentage) => percentage switch
+        {
+            var p when p >= 0 && p < 30 => "H8",
+            var p when p >= 30 && p < 40 => "H7",
+            var p when p >= 40 && p < 50 => "H6",
+            var p when p >= 50 && p < 60 => "H5",
+            var p when p >= 60 && p < 70 => "H4",
+            var p when p >= 70 && p < 80 => "H3",
+            var p when p >= 80 && p < 90 => "H2",
+            var p when p >= 90 && p <= 100 => "H1",
+            _ => "Invalid percentage"
+        };
+
+        static int GetPoints(string grade)
+        {
+            switch(grade)
+            {
+                case "H1":
+                    return 100;
+                case "H2":
+                    return 88;
+                case "H3":
+                    return 77;
+                case "H4":
+                    return 66;
+                case "H5":
+                    return 56;
+                case "H6":
+                    return 46;
+                case "H7":
+                    return 37;
+                case "H8":
+                    return 0;
+                default:
+                    return 0;
+
+            }
+        }
+    }
+}
