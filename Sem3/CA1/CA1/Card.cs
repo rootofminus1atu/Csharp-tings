edef7e7eb39b09f76323a1fd9fa6ff894031f48d
RootOfMinus1ATU <s00240122@atu.ie>
2023-10-26 11:49:52 +0100

    cards done

---
+++ Sem3/CA1/CA1/Card.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    public enum Suit
+    {
+        Diamonds,
+        Hearts,
+        Spades,
+        Clubs
+    }
+
+    public class Card
+    {
+        public ICardValue Value { get; set; }
+        public Suit Suit { get; set; }
+
+
+
+        public interface ICardValue { }
+
+        public class Ace : ICardValue
+        {
+            public override string ToString()
+            {
+                return "Ace";
+            }
+        }
+
+        public class King : ICardValue
+        {
+            public override string ToString()
+            {
+                return "King";
+            }
+        }
+        public class Queen : ICardValue
+        {
+            public override string ToString()
+            {
+                return "Queen";
+            }
+        }
+
+        public class Jack : ICardValue
+        {
+            public override string ToString()
+            {
+                return "Jack";
+            }
+        }
+
+        public class Number : ICardValue
+        {
+            public const int MAX_VAL = 10;
+            public const int MIN_VAL = 2;
+
+            public int Val { get; set; }
+
+            public Number(int value)
+            {
+                Val = value;
+            }
+
+            public override string ToString()
+            {
+                return $"{Val}";
+            }
+        }
+
+
+
+
+        public Card(ICardValue name, Suit suit)
+        {
+            // do a check for valid card here and throw exception maybe
+            Value = name;
+            Suit = suit;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} of {Suit}";
+        }
+
+        public int GetPoints()  // rename to get points
+        {
+            return Value switch
+            {
+                Ace => 11,
+                King or Queen or Jack => 10,
+                Number { Val: int n } when n >= 2 && n <= 10 => n,
+                _ => throw new Exception("No value for this card")
+            };
+        }
+
+
+
+        public static List<ICardValue> GetPictureValues()
+        {
+            return new List<ICardValue>() { new Ace(), new King(), new Queen(), new Jack() };
+        }
+
+        public static IEnumerable<ICardValue> GetNumberValues()
+        {
+            var nums = from num in Enumerable.Range(Number.MIN_VAL, Number.MAX_VAL - 1) select new Number(num);
+            return nums;
+        }
+    }
+}
---
+++ Sem3/CA1/CA1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Net.Http.Headers;
+
+namespace CA1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Card c1 = new(new Card.King(), Suit.Diamonds);
+            Console.WriteLine(c1);
+
+            Deck d1 = new Deck();
+            Console.WriteLine(d1.Cards.Stringify());
+            d1.Shuffle();
+            Console.WriteLine(d1.Cards.Stringify());
+
+            Console.WriteLine("♥ ♣ ♦ ♠ ♤ ♡ ♧ ♢");
+        }
+    }
+
+    public class Game
+    {
+        // contain dealer and player
+        // start and control game
+    }
+
+    public class Player 
+    {
+        
+    }
+
+    public class Dealer
+    {
+
+    }
+
+
+    public class Deck
+    {
+        public List<Card> Cards {  get; set; } = new();
+
+        public Deck()
+        {
+            foreach(Suit s in Enum.GetValues(typeof(Suit)))
+            {
+                // maybe we could use a card factory insted of static methods
+
+                foreach(Card.ICardValue val in Card.GetPictureValues())
+                {
+                    Cards.Add(new Card(val, s));
+                }
+
+                foreach(Card.ICardValue num in Card.GetNumberValues())
+                {
+                    Cards.Add(new Card(num, s));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            var rand = new Random();
+            Cards = Cards.OrderBy(c => rand.Next()).ToList();
+        }
+    }
+
+
+
+
+
+
+    
+
+    
+
+
+    public static class ListExtension
+    {
+        public static string Stringify<T>(this List<T> list)
+        {
+            return $"[ {string.Join(", ", list)} ]";
+        }
+    }
+
+}
