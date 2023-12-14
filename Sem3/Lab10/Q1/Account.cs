987f724207c533f68f2b62b9ee38f8a39a4258a6
RootOfMinus1ATU <s00240122@atu.ie>
2023-12-14 11:54:14 +0000

    create ui

--- Sem3/Lab10/Q1/Account.cs
+++ Sem3/Lab10/Q1/Account.cs
@@ -8,10 +8,20 @@ namespace Q1
 {
     internal abstract class Account
     {
+        public int AccountNumber { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double Balance { get; set; }
         public DateTime InterestDate { get; set; }
+
+        public Account(int accountNumber, string firstName, string lastName, double balance)
+        {
+            AccountNumber = accountNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            Balance = balance;
+            InterestDate = DateTime.Now;
+        }
 
         public void Deposit(double amount)
         {
@@ -30,11 +40,20 @@ namespace Q1
         }
 
         public abstract double CalculateInterest();
+
+        public override string ToString()
+        {
+            return $"{AccountNumber} - {FirstName}, {LastName}";
+        }
     }
 
     internal class CurrentAccount : Account
     {
         public double InterestRate => 0.03;
+
+        public CurrentAccount(int accountNumber, string firstName, string lastName, double balance)
+            : base(accountNumber, firstName, lastName, balance) { }
+
         public override double CalculateInterest()
         {
             // if last year
@@ -51,6 +70,10 @@ namespace Q1
     internal class SavingsAccount : Account
     {
         public double InterestRate => 0.06;
+
+        public SavingsAccount(int accountNumber, string firstName, string lastName, double balance)
+            : base(accountNumber, firstName, lastName, balance) { }
+
         public override double CalculateInterest()
         {
             // if last year
--- Sem3/Lab10/Q1/MainWindow.xaml.cs
+++ Sem3/Lab10/Q1/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace Q1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Account> allAccounts = new();
         public MainWindow()
         {
             InitializeComponent();
@@ -28,8 +29,70 @@ namespace Q1
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
-            Trace.WriteLine("hi");
+            Account a1 = new CurrentAccount(999, "Joe", "Biden", 120);
+            Account a2 = new SavingsAccount(888, "Jeff", "The Killer", 830);
+
+            allAccounts = new List<Account>() { a1, a2 };
+
+
+            cbxCurrentAccounts.IsChecked = true;
+            cbxSavingsAccounts.IsChecked = true;
         }
+
+        private void lbxAccounts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Account? chosenAccount = lbxAccounts.SelectedItem as Account;
+
+            if (chosenAccount == null)
+            {
+                txtFirstName.Text = "";
+                txtLastName.Text = "";
+                txtBalance.Text = "";
+                return;
+            }
+
+            txtFirstName.Text = chosenAccount.FirstName;
+            txtLastName.Text = chosenAccount.LastName;
+            txtBalance.Text = $"{chosenAccount.Balance}";
+        }
+
+        private void ManageCheckboxes()
+        {
+            bool? saChecked = cbxCurrentAccounts.IsChecked;
+            bool? caChecked = cbxSavingsAccounts.IsChecked;
+
+            List<Account> displayedAccounts = (saChecked, caChecked) switch
+            {
+                (false, false or null) => new(),
+                (true, false or null) => allAccounts.Where(a => a is SavingsAccount).ToList(),
+                (false or null, true) => allAccounts.Where(a => a is CurrentAccount).ToList(),
+                (true, true) => allAccounts,
+                _ => new()
+            };
+
+            lbxAccounts.ItemsSource = displayedAccounts;
+            lbxAccounts.Items.Refresh();
+        }
+
+        private void cbxCurrentAccounts_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ManageCheckboxes();
+        }
+
+        private void cbxCurrentAccounts_Checked(object sender, RoutedEventArgs e)
+        {
+            ManageCheckboxes();
+        }
+
+        private void cbxSavingsAccounts_Checked_1(object sender, RoutedEventArgs e)
+        {
+            ManageCheckboxes();
+        }
+
+        private void cbxSavingsAccounts_Unchecked_1(object sender, RoutedEventArgs e)
+        {
+            ManageCheckboxes();
+        }
+
     }
 }
