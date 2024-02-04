f2c41b9a7ff031c6aa7352337e91fcdde9d9d3de
RootOfMinus1ATU <s00240122@atu.ie>
2024-02-04 20:48:44 +0000

    testing

---
+++ Sem4/project-testing/keyboard-sounds/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace keyboard_sounds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to play a sound. Press 'Q' to quit.");
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+
+                PlaySound();
+            }
+        }
+
+        static void PlaySound()
+        {
+            string soundFilePath = "path-to-your-sound-file.wav";
+
+            Console.WriteLine("key got");
+        }
+    }
+}
---
+++ Sem4/project-testing/wpf-attempt-1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_attempt_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly SoundPlayer soundPlayer = new SoundPlayer("vineboom.wav");
+        private bool isListening = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListening_Click(object sender, RoutedEventArgs e)
+        {
+            isListening = !isListening;
+            ((Button)sender).Content = isListening ? "Stop Listening" : "Start Listening";
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            soundPlayer.Dispose();
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Trace.WriteLine("keyyy");
+        }
+    }
+}
