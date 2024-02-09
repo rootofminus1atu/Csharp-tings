cea8014a293579b08656c0a9357543300b1a4571
RootOfMinus1ATU <s00240122@atu.ie>
2024-02-09 20:21:00 +0000

    i hate csha

---
+++ Sem4/chat-keybinds-project/first-attempt/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using GlobalHotKey;
+using NHotkey.Wpf;
+using NHotkey;
+
+namespace first_attempt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string pasteThis = "Hellooo";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPress(object sender, HotkeyEventArgs e)
+        {
+            Trace.WriteLine("WOOOAH");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+            // do this in the combobox
+
+            Trace.WriteLine("WOA");
+
+            var openWindowProcesses = Process.GetProcesses()
+                .Where(p => p.MainWindowHandle != IntPtr.Zero && p.ProcessName != "explorer");
+
+            foreach (var window in openWindowProcesses)
+            {
+                Trace.WriteLine(window.ProcessName);
+            }
+
+            Trace.WriteLine("WOA end");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+
+        private void cbxWindows_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cbxWindows_DropDownOpened(object sender, EventArgs e)
+        {
+            Trace.WriteLine("hii");
+        }
+
+        
+
+        private void btnDoFunny_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("Doing the funnny");
+
+
+
+            Trace.WriteLine("before");
+
+            System.Threading.Thread.Sleep(1000);
+
+            
+
+            Trace.WriteLine("after");
+        }
+
+        private void tabPasteEnter()
+        {
+            SendKeys.SendWait("{TAB}");
+            SendKeys.SendWait(pasteThis);
+            SendKeys.SendWait("{ENTER}");
+        }
+    }
+}
