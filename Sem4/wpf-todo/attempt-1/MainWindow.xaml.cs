0b245b5eeb2aa1eb3a2faacc46fd315a9606d1eb
RootOfMinus1ATU <s00240122@atu.ie>
2024-04-29 09:58:42 +0100

    wow this isnt terrible

---
+++ Sem4/wpf-todo/attempt-1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace attempt_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<TodoItem> todoItems = new ObservableCollection<TodoItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TodoListBox.ItemsSource = todoItems;
+        }
+
+        private void AddTodo_Click(object sender, RoutedEventArgs e)
+        {
+            string newTodoText = NewTodoTextBox.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(newTodoText))
+            {
+                todoItems.Add(new TodoItem { Text = newTodoText });
+                NewTodoTextBox.Clear();
+            }
+        }
+
+        private void RemoveTodo_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (button.DataContext is TodoItem todoItem)
+                {
+                    todoItems.Remove(todoItem);
+                }
+            }
+        }
+    }
+}
---
+++ Sem4/wpf-todo/attempt-1/TodoItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attempt_1
+{
+    internal class TodoItem : INotifyPropertyChanged
+    {
+        private string text;
+        public string Text
+        {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+
+        private bool isCompleted;
+        public bool IsCompleted
+        {
+            get => isCompleted;
+            set => SetProperty(ref isCompleted, value);
+        }
+
+
+        // damn so much boilerplate
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
