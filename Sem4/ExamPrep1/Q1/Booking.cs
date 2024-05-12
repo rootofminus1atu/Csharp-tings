4b5719888bbc4a67e2b6163b47a0fe7ff4a85df2
RootOfMinus1ATU <s00240122@atu.ie>
2024-05-13 00:08:43 +0100

    examprep

---
+++ Sem4/ExamPrep1/Q1/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfTickets { get; set; }
+
+        public int MovieID { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
---
+++ Sem4/ExamPrep1/Q1/MovieData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    public class MovieData : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+
+        public MovieData() : base("OODExam_NameSurname") { }
+    }
+}
