0ef33233caff104edce49432c3b9480dc9cf07e6
RootOfMinus1ATU <s00240122@atu.ie>
2024-05-13 12:01:48 +0100

    all

--- Sem4/ExamPrep1/Q1/MainWindow.xaml.cs
+++ Sem4/ExamPrep1/Q1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,7 @@ namespace Q1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private MovieData db = new MovieData();
+        private const int MAX_SEATS = 100;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,9 +31,27 @@ namespace Q1
 
         private void LoadMovies()
         {
-            using(db)
+            using (var db = new MovieData())
             {
                 lbxMovies.ItemsSource = db.Movies.ToList();
+            }
+        }
+
+        private void RefreshAvailableSeats(int availableSeats)
+        {
+            // update the number based on how many bookings there are
+            int availableSeatsNum = MAX_SEATS - availableSeats;
+            txtAvailableSeats.Text = $"{availableSeatsNum}"; ;
+        }
+
+        private int GetAvailableSeats(Movie movie, DateTime date)
+        {
+            using (var db = new MovieData())
+            {
+                return db.Bookings
+                    .Where(b => b.MovieID == movie.MovieID && b.BookingDate == date)
+                    .Select(b => b.NumberOfTickets)
+                    .Sum();
             }
         }
 
@@ -41,10 +60,66 @@ namespace Q1
             if (lbxMovies.SelectedItem is Movie selectedMovie)
             {
                 txtDescription.Text = selectedMovie.Description;
+
+                if (dpDate.SelectedDate is DateTime selectedDate)
+                {
+                    RefreshAvailableSeats(GetAvailableSeats(selectedMovie, selectedDate));
+                }
             } 
             else
             {
                 txtDescription.Text = string.Empty;
+            }
+        }
+
+        private void dpDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbxMovies.SelectedItem is Movie selectedMovie && dpDate.SelectedDate is DateTime selectedDate)
+            {
+                RefreshAvailableSeats(GetAvailableSeats(selectedMovie, selectedDate));
+            }
+        }
+
+        private void btnBookSeats_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxMovies.SelectedItem is Movie selectedMovie && dpDate.SelectedDate is DateTime selectedDate)
+            {
+                bool parsingRes = int.TryParse(tblkRequiredSeats.Text, out int requiredSeats);
+
+                if (!parsingRes)
+                {
+                    MessageBox.Show("no strings have a number in the requiredSeats");
+                    return;
+                }
+
+                if (requiredSeats <= 0)
+                {
+                    MessageBox.Show("you can't book 0 or a negative number of seats");
+                }
+
+                int availableSeats = GetAvailableSeats(selectedMovie, selectedDate);
+
+                if (availableSeats - requiredSeats < 0)
+                {
+                    MessageBox.Show("too many seats");
+                    return;
+                }
+
+                var newBooking = new Booking { BookingDate =  selectedDate, NumberOfTickets = requiredSeats, MovieID = selectedMovie.MovieID };
+                using (var db = new MovieData())
+                {
+                    db.Bookings.Add(newBooking);
+                    db.SaveChanges();
+                }
+
+                MessageBox.Show("Booking made");
+
+                LoadMovies();
+                RefreshAvailableSeats(availableSeats);
+            }
+            else
+            {
+                MessageBox.Show("Select a movie AND a date please");
             }
         }
     }
